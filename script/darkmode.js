c79bb1c46217370a56182b24fd6f7f5f22d55ba8
Hakeem <hakeemsharif5@gmail.com>
2023-10-09 21:59:24 +0300

    Update 1.0.17i
    Dark Mode Script Adjust

--- script/darkmode.js
+++ script/darkmode.js
@@ -28,6 +28,7 @@ const enableIcon = () => {
 
 const disableDarkMode = () => {
   // 1. Remove the class from the body
+  //document.body.classList.remove('dark-theme');
   document.documentElement.classList.remove('dark-theme');
   document.getElementById('dark-mode-toggle').src = "files/icons/Dark.png";
   // 2. Update darkMode in localStorage 
