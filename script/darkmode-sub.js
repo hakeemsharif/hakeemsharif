be8d6f0713ae7ce3dfd61b5dfac410d70bf2f9f7
Hakeem <hakeemsharif5@gmail.com>
2023-10-09 15:47:58 +0300

    Update 1.0.1
    Reverted Update 1.0 to fixed folder name.

---
+++ script/darkmode-sub.js
@@ -0,0 +1,75 @@
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem('dark-theme'); 
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+//var icon = document.getElementById("mainlogo");
+let icon = localStorage.getItem('mainlogo');
+let homeicon = localStorage.getItem('homeicon');
+
+const enableDarkMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add('dark-theme');
+  document.getElementById('dark-mode-toggle').src = "../files/icons/Light.png";
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('dark-theme', 'enabled');
+}
+
+const enableIcon = () => {
+    // 1. Add the class to the body
+    
+    document.getElementById('mainlogo').src = "../files/hs-logo/LogoLight.png";
+    document.getElementById('homeicon').src = "../files/icons/home-light.png";
+    // 2. Update darkMode in localStorage
+    localStorage.setItem("mainlogo", 'enabled');
+  }
+
+
+const disableDarkMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove('dark-theme');
+  document.getElementById('dark-mode-toggle').src = "../files/icons/Dark.png";
+  // 2. Update darkMode in localStorage 
+  localStorage.setItem('dark-theme', null);
+}
+
+const disableIcon = () => {
+    // 1. Add the class to the body
+    
+    document.getElementById('mainlogo').src = "../files/hs-logo/LogoMain.png";
+    document.getElementById('homeicon').src = "../files/icons/home-dark.png";
+    // 2. Update darkMode in localStorage
+    localStorage.setItem("mainlogo", 'enabled');
+  }
+ 
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+  enableDarkMode();
+  enableIcon();
+}
+
+
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+  // get their darkMode setting
+  darkMode = localStorage.getItem('dark-theme'); 
+  
+  // if it not current enabled, enable it
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+    enableIcon();
+    console.log("enabled")
+    //icon.src = "files/hs-logo/LogoLight.png";
+
+
+  // if it has been enabled, turn it off  
+  } else {  
+    disableDarkMode(); 
+    disableIcon();
+    //icon.src = "files/hs-logo/LogoMain.png";
+  }
+});
+
+
